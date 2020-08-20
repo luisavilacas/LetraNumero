17066f652183f2bcf34e4f57b22548122d43239a
Luis Avila <a2018117998@estudiantes.upsa.edu.bo>
2020-08-19 20:02:40 -0400

    Verificacion del codigo

---
+++ NumeroaLetra/NumeroaLetra/Controllers/LiteralController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NumeroaLetra.Helpers;
+
+namespace NumeroaLetra.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LiteralController : ControllerBase
+    {
+        [HttpGet]
+        public string Literalget(int numero)
+        {
+            return Conversores.NumeroALetras(numero);
+        }
+        [HttpPost]
+        public string LiteralPost([FromHeader] int numero)
+        {
+            return Conversores.NumeroALetras(numero);
+
+        }
+    }
+}
